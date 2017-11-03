15d1d35dfb22a801a1947ef3e3b76f36ad990d02
mysterism <mysterism9@gmail.com>
2017-11-04 01:37:23 +0900

    Add page_main locale

--- i18n.data.ts
+++ i18n.data.ts
@@ -13,6 +13,118 @@ const root: Root = {
     "@ko": "파워",
     "@en": "1"
   },
+  "definition.suggestion": {
+    "@ko": "혹시 다음을 검색하고 싶으신가요?",
+    "@en": "Searching for these?"
+  },
+  "definition.timeSince": {
+    "@ko": "주",
+    "@en": "w"
+  },
+  "definition.partOfSpeech": {
+    "@ko": "품사",
+    "@en": "part of speech"
+  },
+  "definition.partOfSpeech.noun": {
+    "@ko": "명사",
+    "@en": "noun"
+  },
+  "definition.partOfSpeech.verb": {
+    "@ko": "동사",
+    "@en": "verb"
+  },
+  "definition.partOfSpeech.adjective": {
+    "@ko": "형용사",
+    "@en": "adjective"
+  },
+  "definition.bubbleMenu.favorite": {
+    "@ko": "관심 글로 등록하기",
+    "@en": "Save this post as my favorite"
+  },
+  "definition.bubbleMenu.report": {
+    "@ko": "이 글 신고하기",
+    "@en": "Report this post"
+  },
+  "definition.bubbleMenu.edit": {
+    "@ko": "글 편집하기",
+    "@en": "Edit post"
+  },
+  "definition.bubbleMenu.delete": {
+    "@ko": "삭제하기",
+    "@en": "Delete post"
+  },
+  "definition.commentFilter.hot": {
+    "@ko": "추천순",
+    "@en": "Hot"
+  },
+  "definition.commentFilter.recent": {
+    "@ko": "최신순",
+    "@en": "Recent"
+  },
+  "definition.writeComment": {
+    "@ko": "덧글 쓰기",
+    "@en": "Add comment"
+  },
+  "definition.seeMoreComments": {
+    "@ko": "덧글 더 보기",
+    "@en": "See more"
+  },
+  "definition.termRegister": {
+    "@ko": "말 올리기",
+    "@en": "Register New Term"
+  },
+  "definition.registeringTerm": {
+    "@ko": "올릴 말",
+    "@en": "Registering Term"
+  },
+  "definition.meaning": {
+    "@ko": "뜻",
+    "@en": "Meaning"
+  },
+  "definition.registeringTerm.instruction": {
+    "@ko": "등록할 말을 적어주세요 (띄어 쓰기 포함 28자 이내)",
+    "@en": "Please enter a new term (within 28 characters)"
+  },
+  "definition.meaning.instruction": {
+    "@ko": "등록할 말의 뜻을 적어주세요",
+    "@en": "Please enter meaning of the new term"
+  },
+  "definition.usage": {
+    "@ko": "쓰임새",
+    "@en": "Usage"
+  },
+  "definition.usage.instruction": {
+    "@ko": "말의 쓰임새를 적어주세요 (ex. 이거 실화임?)",
+    "@en": "Please enter usage of the new term"
+  },
+  "definition.usage.add": {
+    "@ko": "쓰임새 추가하기",
+    "@en": "Add Usage"
+  },
+  "definition.referenceImage": {
+    "@ko": "참조 이미지",
+    "@en": "Reference Image"
+  },
+  "definition.referenceImage.attach": {
+    "@ko": "이곳을 클릭하거나 파일을 끌어 놓으세요",
+    "@en": "Click or drag image file here"
+  },
+  "definition.tag": {
+    "@ko": "태그",
+    "@en": "Tags"
+  },
+  "definition.taggingInstruction": {
+    "@ko": "#연관된 단어를 태그해주세요",
+    "@en": "#TagRelatedTerm"
+  },
+  "definition.addMoreTerm": {
+    "@ko": "말 더 등록하기",
+    "@en": "Add More Term"
+  },
+  "definition.done": {
+    "@ko": "완성",
+    "@en": "Done"
+  }
 }
 
 export default root;
