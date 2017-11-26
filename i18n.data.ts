89e831319052b748a45872d5ea733bc39007fd5d
mysterism <mysterism9@gmail.com>
2017-11-26 21:06:48 +0900

    Add hamburger menu locale

--- i18n.data.ts
+++ i18n.data.ts
@@ -37,94 +37,128 @@ const root: Root = {
     "@ko": "형용사",
     "@en": "adjective"
   },
-  "definition.bubbleMenu.favorite": {
+  "bubbleMenu.favorite": {
     "@ko": "관심 글로 등록하기",
     "@en": "Save this post as my favorite"
   },
-  "definition.bubbleMenu.report": {
+  "bubbleMenu.report": {
     "@ko": "이 글 신고하기",
     "@en": "Report this post"
   },
-  "definition.bubbleMenu.edit": {
+  "bubbleMenu.edit": {
     "@ko": "글 편집하기",
     "@en": "Edit post"
   },
-  "definition.bubbleMenu.delete": {
+  "bubbleMenu.delete": {
     "@ko": "삭제하기",
     "@en": "Delete post"
   },
-  "definition.commentFilter.hot": {
+  "comment.commentFilter.hot": {
     "@ko": "추천순",
     "@en": "Hot"
   },
-  "definition.commentFilter.recent": {
+  "comment.commentFilter.recent": {
     "@ko": "최신순",
     "@en": "Recent"
   },
-  "definition.writeComment": {
+  "comment.writeComment": {
     "@ko": "덧글 쓰기",
     "@en": "Add comment"
   },
-  "definition.seeMoreComments": {
+  "comment.seeMoreComments": {
     "@ko": "덧글 더 보기",
     "@en": "See more"
   },
-  "definition.termRegister": {
+  "newTerm.termRegister": {
     "@ko": "말 올리기",
     "@en": "Register New Term"
   },
-  "definition.registeringTerm": {
+  "newTerm.registeringTerm": {
     "@ko": "올릴 말",
     "@en": "Registering Term"
   },
-  "definition.meaning": {
+  "newTerm.meaning": {
     "@ko": "뜻",
     "@en": "Meaning"
   },
-  "definition.registeringTerm.instruction": {
+  "newTerm.registeringTerm.instruction": {
     "@ko": "등록할 말을 적어주세요 (띄어 쓰기 포함 28자 이내)",
     "@en": "Please enter a new term (within 28 characters)"
   },
-  "definition.meaning.instruction": {
+  "newTerm.meaning.instruction": {
     "@ko": "등록할 말의 뜻을 적어주세요",
     "@en": "Please enter meaning of the new term"
   },
-  "definition.usage": {
+  "newTerm.usage": {
     "@ko": "쓰임새",
     "@en": "Usage"
   },
-  "definition.usage.instruction": {
+  "newTerm.usage.instruction": {
     "@ko": "말의 쓰임새를 적어주세요 (ex. 이거 실화임?)",
     "@en": "Please enter usage of the new term"
   },
-  "definition.usage.add": {
+  "newTerm.usage.add": {
     "@ko": "쓰임새 추가하기",
     "@en": "Add Usage"
   },
-  "definition.referenceImage": {
+  "newTerm.referenceImage": {
     "@ko": "참조 이미지",
     "@en": "Reference Image"
   },
-  "definition.referenceImage.attach": {
+  "newTerm.referenceImage.attach": {
     "@ko": "이곳을 클릭하거나 파일을 끌어 놓으세요",
     "@en": "Click or drag image file here"
   },
-  "definition.tag": {
+  "newTerm.tag": {
     "@ko": "태그",
     "@en": "Tags"
   },
-  "definition.taggingInstruction": {
+  "newTerm.taggingInstruction": {
     "@ko": "#연관된 단어를 태그해주세요",
     "@en": "#TagRelatedTerm"
   },
-  "definition.addMoreTerm": {
+  "newTerm.addMoreTerm": {
     "@ko": "말 더 등록하기",
     "@en": "Add More Term"
   },
-  "definition.done": {
+  "newTerm.done": {
     "@ko": "완성",
     "@en": "Done"
-  }
+  },
+  "comment.commentInstruction": {
+    "@ko": "댓글을 입력해주세요.",
+    "@en": "Write comment here."
+  },
+  "comment.postComment": {
+    "@ko": "댓글 등록",
+    "@en": "Post"
+  },
+  "hamburger.signIn": {
+    "@ko": "로그인",
+    "@en": "Sign in"
+  },
+  "hamberger.feedback": {
+    "@ko": "사용자 의견"
+    "@en": "Feedback"
+  },
+  "hamberger.appInfo": {
+    "@ko": "말모음이란?"
+    "@en": "What is Malmoym?"
+  },
+  "hamberger.back": {
+    "@ko": "취소"
+    "@en": "Back"
+  },
+  "hamberger.signOut": {
+    "@ko": "로그아웃"
+    "@en": "Sign Out"
+  },
+  "hamberger.myAccount": {
+    "@ko": "나의 계정"
+    "@en": "My Account"
+  },
+
+
 }
 
 export default root;
